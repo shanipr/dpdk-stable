DPDK/dpdk

commit 280d641cf8d867695326342bed4145c615d52912

net/hns3: fix resources release on reset
[ upstream commit 361eab82df67c09cb84a9e2e66c0d93a84be610d ]

Some resources, like, unmapping Rx interrupt, doesn't perform
when execute dev_stop on reset. This will lead to other issues.

Fixes: 2790c6464725 ("net/hns3: support device reset")

Signed-off-by: Dengdui Huang <huangdengdui@huawei.com>

drivers/net/hns3/hns3_ethdev.c
@@ -5219,20 +5219,23 @@ hns3_dev_stop(struct rte_eth_dev *dev)
 	struct hns3_hw *hw = &hns->hw;
 
 	PMD_INIT_FUNC_TRACE();
+	if (rte_atomic_load_explicit(&hw->reset.resetting, rte_memory_order_relaxed) != 0) {
+		hns3_warn(hw, "device is resetting, stop operation is not allowed.");
+		return -EBUSY;
+	}
+
 	dev->data->dev_started = 0;
 
 	hw->adapter_state = HNS3_NIC_STOPPING;
 	hns3_stop_rxtx_datapath(dev);
 
 	rte_spinlock_lock(&hw->lock);
-	if (__atomic_load_n(&hw->reset.resetting, __ATOMIC_RELAXED) == 0) {
-		hns3_tm_dev_stop_proc(hw);
-		hns3_config_mac_tnl_int(hw, false);
-		hns3_stop_tqps(hw);
-		hns3_do_stop(hns);
-		hns3_unmap_rx_interrupt(dev);
-		hw->adapter_state = HNS3_NIC_CONFIGURED;
-	}
+	hns3_tm_dev_stop_proc(hw);
+	hns3_config_mac_tnl_int(hw, false);
+	hns3_stop_tqps(hw);
+	hns3_do_stop(hns);
+	hns3_unmap_rx_interrupt(dev);
+	hw->adapter_state = HNS3_NIC_CONFIGURED;
 	hns3_rx_scattered_reset(dev);
 	rte_eal_alarm_cancel(hns3_service_handler, dev);
 	hns3_stop_report_lse(dev);

shanipr/dpdk-stable

commit 2e0e76f08571c6d1b64d8e76f70b1803cf087f59

net/hns3: fix atomic API usage for LTS compatibility
The rte_atomic_load_explicit() API requires the variable to be
declared with RTE_ATOMIC() type. However, hw->reset.resetting is
declared as plain uint16_t, causing a compilation error with clang.

Revert to using __atomic_load_n() which is consistent with all other
atomic operations on the resetting field in this driver.

Fixes: 280d641cf8d8 ("net/hns3: fix resources release on reset")
Cc: huangdengdui@huawei.com

Signed-off-by: Shani Peretz <shperetz@nvidia.com>
Acked-by: Kevin Traynor <ktraynor@redhat.com>

drivers/net/hns3/hns3_ethdev.c
@@ -5230,7 +5230,7 @@ hns3_dev_stop(struct rte_eth_dev *dev)
 	struct hns3_hw *hw = &hns->hw;
 
 	PMD_INIT_FUNC_TRACE();
-	if (rte_atomic_load_explicit(&hw->reset.resetting, rte_memory_order_relaxed) != 0) {
+	if (__atomic_load_n(&hw->reset.resetting, __ATOMIC_RELAXED) != 0) {
 		hns3_warn(hw, "device is resetting, stop operation is not allowed.");
 		return -EBUSY;
 	}